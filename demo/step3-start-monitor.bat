bae8beac62c1cd9c003d5cc5c675ffbc0d6bff7d
world <world@peace.com>
2025-05-10 08:54:06 +0800

    no message

--- demo/step3-start-monitor.bat
+++ demo/step3-start-monitor.bat
@@ -1,5 +1,5 @@
-echo "go install github.com/zxfonline/expvarmon@latest"
-go install github.com/zxfonline/expvarmon@latest
+echo "go install github.com/divan/expvarmon@latest"
+go install github.com/divan/expvarmon@latest
 
 expvarmon -ports="http://localhost:1234/debug/pprof/vars" -vars="mem:memstats.Alloc,Goroutines,tracetotal.Actor1.Count,duration:tracetotal.Actor1.Mean,duration:tracetotal.Actor1.StdDev,duration:tracetotal.Actor1.Median,mem:memstats.Sys,mem:memstats.HeapAlloc,mem:memstats.HeapInuse,memstats.EnableGC,memstats.NumGC,duration:memstats.PauseNs,duration:Uptime,chanstats.chanMainProc.Len,chanstats.chanMainProc.Rate" -i=1s
 
--- nats-server/cluster1.bat
+++ nats-server/cluster1.bat
@@ -1,3 +1,4 @@
+go install github.com/nats-io/nats-server/v2@main
 start nats-server.exe -c c1js11.conf
 start nats-server.exe -c c1js12.conf
 start nats-server.exe -c c1js13.conf
